bd4250d10d14349bea7e5043b3e33afd53dc785d
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2023-03-13 22:23:53 +0500

    edit: change fizika jsx file

--- src/pages/dashboard/fizika.jsx
+++ src/pages/dashboard/fizika.jsx
@@ -70,9 +70,9 @@ function Fizika() {
             height="400"
             src="https://www.youtube.com/embed/j-OKVREJ3AE"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
 
